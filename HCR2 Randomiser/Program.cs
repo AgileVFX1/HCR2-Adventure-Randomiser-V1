ce47e324fa35e87b89b1ab0e292cbfbc12b846c5
AgileVFX1 <125081821+AgileVFX1@users.noreply.github.com>
2023-06-20 19:38:25 +0100

    Add project files.

---
+++ HCR2 Randomiser/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCR2_Randomiser
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" _Randomised HCR2_");
+            Console.WriteLine("");
+
+            List<string> ChosenCar = new List<string>() {
+
+            "Hill Climber",
+            "Scooter",
+            "Bus",
+            "Hill Climber MK2",
+            "Tractor",
+            "Motocross",
+            "Dune Buggy",
+            "Sports Car",
+            "Monster Truck",
+            "Rotator",
+            "Super Diesel",
+            "Chopper",
+            "Tank",
+            "Lowrider",
+            "Snowmobile",
+            "Monowheel",
+            "Beast",
+            "Rally Car",
+            "Formula",
+            "Muscle Car",
+            "Racing Truck",
+            "Hot Rod",
+            "CC - EV",
+            "Superbike",
+            "Supercar",
+            "Moonlander",
+             };
+
+            Random RandCar = new Random();
+            int CarIndex = RandCar.Next(ChosenCar.Count);
+            string RandomisedCar = ChosenCar[CarIndex];
+            ChosenCar.RemoveAt(CarIndex);
+
+            Console.WriteLine($" Chosen car: {RandomisedCar}");
+            Console.WriteLine("");
+
+            //-----------------------------------------------------
+
+            List<string> ChosenMap = new List<string>() {
+            "Countryside",
+            "Forest",
+            "City",
+            "Mountain",
+            "Rustbucket Reef",
+            "Winter",
+            "Mines",
+            "Desert Valley",
+            "Beach",
+            "Backwater Bog",
+            "Racer Glacier",
+            "Patchwork Plant",
+            "Gloomvale",
+            "Overspill Fun Rig",
+            "Canyon Arena",
+             }; // you can add more maps following the same format
+
+            Random RandMap = new Random();
+            int MapIndex = RandMap.Next(ChosenMap.Count);
+            string RandomisedMap = ChosenMap[MapIndex];
+            ChosenMap.RemoveAt(MapIndex);
+
+            Console.WriteLine($" Chosen Map: {RandomisedMap}");
+            Console.WriteLine("");
+
+            //-----------------------------------------------------
+
+            List<int> NumOfParts = new List<int>() { 0, 1, 2, 3 };
+
+            Random RandNumOfParts = new Random();
+            int PartNumIndex = RandNumOfParts.Next(NumOfParts.Count);
+
+            Console.WriteLine($" Number Of Parts: {PartNumIndex}");
+            Console.WriteLine("");
+
+            int MaxParts = PartNumIndex;
+
+            List<string> ListOfParts = new List<string>{
+            "Magnet",
+            "Heavyweight",
+            "Wings",
+            "Rollcage",
+            "Air Control",
+            "Winter Tires",
+            "Start Boost",
+            "Wheelie Boost",
+            "Fume Boost",
+            "Flip Boost",
+            "Jump Shocks",
+            "Landing Boost",
+            "Overcharged Turbo",
+            "Afterburner",
+            "Spoiler",
+            "Thrusters",
+            "Fuel Boost",
+            "Coin Boost",
+             };
+
+            List<string> PickedParts = new List<string>();
+
+            for (int i = 0; i < MaxParts; i++)
+            {
+                int randomIndex = RandNumOfParts.Next(0, NumOfParts.Count);
+                string PickedPart = ListOfParts[randomIndex];
+                PickedParts.Add(PickedPart);
+                ListOfParts.RemoveAt(randomIndex);
+            }
+
+            Console.WriteLine(" Picked parts: ");
+
+            foreach (var PickedPart in PickedParts)
+            {
+                Console.WriteLine(" - " + PickedPart);
+            }
+
+            Console.WriteLine("\n _Made By AgileVFX for Patbrick_");
+            Console.ReadKey();
+        }
+    }
+}
